8a2503783b2c5a38f2be60ab3da330812e773880
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-10 16:22:03 +0100

    added seventh snack

---
+++ src/org/lessons/java/Snack7.java
@@ -0,0 +1,16 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack7 {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Inserisci il numero di secondi: ");
+		
+		int seconds = in.nextInt();
+	}
+
+}
