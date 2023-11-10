9f9a6aca5a14cab9391b7f5164923e53aec5d943
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-10 11:38:56 +0100

    added first snack

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,9 +1,22 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 	
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Inserisci un numero: ");
+		int number = in.nextInt();
+		int nextnumber = number + 1;
+		
+		if(number %2 == 0) {
+			System.out.println("Il numero è pari: " + number);
+		} else {
+			System.out.println("Il numero è dispari, il successivo è " + nextnumber);
+		}
+			
 	}
 
 }
