564dd167a76b100793f4e8e500a00683eda9fd11
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-10 16:29:35 +0100

    fixed sixth snack

--- src/org/lessons/java/Snack6.java
+++ src/org/lessons/java/Snack6.java
@@ -4,28 +4,25 @@ public class Snack6 {
 	
 	public static void main(String[] args) {
 		   String numberString = "1996";
-
-	        int result = convertToInteger(numberString);
-
-	        System.out.println("Il numero convertito è: " + result);
-	};
+		   
+		   int length = numberString.length();
+		   
+		   int result = 0;
+		   
+		   for (int x = 0; x < length; x++) {
+			   char n = numberString.charAt(x);
+			   
+			   if(n >= '0' && n <= '9') {
+				   int number = n - '0';
+				   result = result * 10 + number;
+			   }
+		   }
+		   
+		   System.out.println("Integer: " + result);
+		   System.out.println("String: " + numberString);
+		   
 	
-    private static int convertToInteger(String numberString) {
-        int length = numberString.length();
-        int result = 0;
-
-        for (int i = 0; i < length; i++) {
-            char character = numberString.charAt(i);
-
-            // Verifico se il carattere è un numero
-            if (character >= '0' && character <= '9') {
-                int numericValue = character - '0';
-                result = result * 10 + numericValue;
-            }
-        }
-
-        return result;
-    }
+	}
 	
 
 }
--- src/org/lessons/java/Snack7.java
+++ src/org/lessons/java/Snack7.java
@@ -10,7 +10,18 @@ public class Snack7 {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Inserisci il numero di secondi: ");
 		
-		int seconds = in.nextInt();
+		int second = in.nextInt();
+		
+        // Calcolo ore, minuti e secondi
+        int hours = second / 3600;
+        int minutes = (second % 3600) / 60;
+        int seconds = second % 60;
+
+        // Formatto l'output come "hh:mm:ss"
+        String result = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+        // Mostro il risultato
+        System.out.println("Il tempo convertito è: " + result);
 	}
 
 }
